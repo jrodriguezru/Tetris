058735aa0a7811dd9b4e06c8ae747e94c2bfbd92
jrodriguezru <jrodriguezru@unal.edu.co>
2021-11-02 18:02:30 -0500

    Completed L2 and changed rotation control

--- sketch.js
+++ sketch.js
@@ -8,8 +8,8 @@
 let height = 400;
 let width = 280;
 
-let infoDeL1 = [20, 20, 0, 0, height - 60, 0, height - 40, 20, height - 60, 0, height - 6, width - 40, width - 60, width - 60, width - 60];
-let infoDeL2 = [40, 100, 0];
+let infoDeL1 = [20, 20, 0, 0, height - 60, 0, height - 40, 20, height - 60, 0, height - 60, width - 40, width - 60, width - 60, width - 60];
+let infoDeL2 = [40, 100, 0, 0, height - 60, 20, height - 60, 20, height - 60, 20, height - 40, width - 40, width - 40, width - 20, width - 40];
 let infoDeI = [60, 220, 0];
 let infoDeCuadrado = [100, 20, 0];
 let infoDeS = [100, 120, 0];
@@ -280,6 +280,6 @@ function keyPressed() {
     }
   }
   if (keyCode == UP_ARROW) {
-    infoDeL2[2] += 1;
+    infoDeI[2] += 1;
   }
 }
