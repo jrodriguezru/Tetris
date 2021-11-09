7ab71e314df24c64395f84822969cbd3ee85bddc
juanrr8765 <juanrr8765@gmail.com>
2021-11-09 12:27:06 -0500

    Added objects version of project and objects + bitwise

---
+++ sketchWithObjects.js
@@ -0,0 +1,138 @@
+let height = 400;
+let width = 280;
+
+//let infoDeL1 = [];
+
+let L1 = {posEnX : 20,
+          posEnY : 20,
+          rotation : 0,
+          limiteXR0 : 0,
+          limiteY0 : height - 60,
+          limiteXR1 : 0,
+          limiteY1 : height - 40,
+          limiteXR2 : 20,
+          limiteY2 : height - 60,
+          limiteXR3 : 0,
+          limiteYR : height - 60,
+          limiteXI0 : width - 40,
+          limiteXI1 : width - 60,
+          limiteXI2 : width - 60,
+          limiteXI3 :width - 60,
+
+        };
+
+//let infoDeL2 = [];
+
+let l2 = {posEnX : 40,
+          posEnY : 100,
+          rotation : 0,
+          limiteXR0 : 0,
+          limiteY0 : height - 60,
+          limiteXR1 : 20,
+          limiteY1 : height - 60,
+          limiteXR2 : 20,
+          limiteY2 : height - 60,
+          limiteXR3 : 20,
+          limiteYR : height - 40,
+          limiteXI0 : width - 40,
+          limiteXI1 : width - 40,
+          limiteXI2 : width - 20,
+          limiteXI3 : width - 40,
+          }; 
+
+//let infoDeI = [];
+
+let I = {posEnX : 60,
+    posEnY : 220,
+    rotation : 0,
+    limiteXR0 : 0,
+    limiteY0 : height - 80,
+    limiteXR1 : 40,
+    limiteY1 : height - 60,
+    limiteXR2 : 20,
+    limiteY2 : height - 80,
+    limiteXR3 : 40,
+    limiteYR : height - 60,
+    limiteXI0 : width - 20,
+    limiteXI1 : width - 40,
+    limiteXI2 : width,
+    limiteXI3 : width - 20,
+    };
+
+//let infoDeCuadrado = [];
+
+let Cuad = {posEnX : 100,
+    posEnY : 20,
+    rotation : 0,
+    limiteXR0 : 0,
+    limiteY0 : height - 40,
+    limiteXR1 : 0,
+    limiteY1 : height - 40,
+    limiteXR2 : 0,
+    limiteY2 : height - 40,
+    limiteXR3 : 0,
+    limiteYR : height - 40,
+    limiteXI0 : width - 40,
+    limiteXI1 : width - 40,
+    limiteXI2 : width - 40,
+    limiteXI3 : width - 40,
+    };
+
+//let infoDeS = [];
+
+let S = {posEnX : 100,
+    posEnY : 120,
+    rotation : 0,
+    limiteXR0 : 0,
+    limiteY0 : height - 20,
+    limiteXR1 : 0,
+    limiteY1 : height - 40,
+    limiteXR2 : 0,
+    limiteY2 : height - 20,
+    limiteXR3 : 0,
+    limiteYR : height - 40,
+    limiteXI0 : width - 60,
+    limiteXI1 : width - 40,
+    limiteXI2 : width - 60,
+    limiteXI3 : width - 40,
+    };
+
+//let infoDeZ = [];
+
+let Z = {posEnX : 100,
+    posEnY : 180,
+    rotation : 0,
+    limiteXR0 : 0,
+    limiteY0 : height - 40,
+    limiteXR1 : 0,
+    limiteY1 : height - 60,
+    limiteXR2 : 0,
+    limiteY2 : height - 40,
+    limiteXR3 : 0,
+    limiteYR : height - 60,
+    limiteXI0 : width - 60,
+    limiteXI1 : width - 40,
+    limiteXI2 : width - 60,
+    limiteXI3 : width - 40
+    };
+
+//let infoDeT = [];
+
+let T = {posEnX : 220,
+    posEnY : 120,
+    rotation : 0,
+    limiteXR0 : 0,
+    limiteY0 : height - 20,
+    limiteXR1 : 20,
+    limiteY1 : height - 40,
+    limiteXR2 : 0,
+    limiteY2 : height - 40,
+    limiteXR3 : 0,
+    limiteYR : height - 40,
+    limiteXI0 : width - 60,
+    limiteXI1 : width - 60,
+    limiteXI2 : width - 60,
+    limiteXI3 : width - 40,
+    };
+
+
